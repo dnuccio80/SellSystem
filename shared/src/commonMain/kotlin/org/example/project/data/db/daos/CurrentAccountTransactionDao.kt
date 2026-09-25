a63d464bb728108f419936ccc1f66f04e7db335d
dnuccio80 <nuccio227@gmail.com>
2026-09-24 21:44:49 -0300

    - Deleted unused ClientWithTransactionsData.kt - Deleted unused imports on CurrentAccountTransactionDao.kt

--- shared/src/commonMain/kotlin/org/example/project/data/db/daos/CurrentAccountTransactionDao.kt
+++ shared/src/commonMain/kotlin/org/example/project/data/db/daos/CurrentAccountTransactionDao.kt
@@ -7,15 +7,12 @@ import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 import org.example.project.data.db.entities.CurrentAccountTransactionEntity
-import org.example.project.data.db.entities.relations.ClientWithTransactionsData
-import org.example.project.domain.models.currentaccount.ClientWithTransactions
 
 @Dao
 interface CurrentAccountTransactionDao {
 
     @Query("SELECT * FROM CurrentAccountTransactionEntity WHERE clientId = :clientId ORDER BY date, id DESC")
     fun getTransactions(clientId:Int): Flow<List<CurrentAccountTransactionEntity>>
-//    fun getTransactions(clientId:Int): Flow<List<ClientWithTransactionsData>>
 
     @Insert(onConflict = REPLACE)
     suspend fun addTransaction(transaction: CurrentAccountTransactionEntity)
